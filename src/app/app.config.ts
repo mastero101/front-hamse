16897cfc333f5a374a75245dfd7de0a851a72ea4
mastero101 <mastershift_101@hotmail.com>
2025-07-22 18:36:28 -0600

    feat(i18n): add Spanish locale support for Angular app
    Register Spanish locale data and set it as default in the app configuration. This enables proper localization for Spanish-speaking users.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import localeEsMX from '@angular/common/locales/es-MX';
 
+registerLocaleData(localeEs, 'es');
 registerLocaleData(localeEsMX, 'es-MX');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    MatSnackBarModule
+    MatSnackBarModule,
+    { provide: LOCALE_ID, useValue: 'es' }
   ]
 };
